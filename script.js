df5d375396c81d0ae31597ce07a53e4fd59cdb83
johnysantos22 <santosjohny302@gmail.com>
2025-06-25 21:52:59 -0300

    criação do botão VER MAIS e ultilizando JS para funcionalidade

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function irPara(pagina) {
-document.getElementById('conteudo').innerHTML = `Carregando ${pagina}...`
-}
+window.addEventListener('DOMContentLoaded', () => {
+    const imagens = document.querySelectorAll('.imgs img')
+    const botao = document.getElementById('button')
+
+    imagens.forEach((img, index) => {
+        if (index >= 6) {
+            img.classList.add('hidden');
+        }
+    })
+
+    botao.addEventListener('click', () => {
+        imagens.forEach((img, index) => {
+            if (index >= 6) {
+                img.classList.remove('hidden')
+            }
+        })
+
+        botao.style.display = 'none';
+    })
+})
